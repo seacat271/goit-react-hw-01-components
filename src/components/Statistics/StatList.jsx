2707a9a945ac7f7dd3f8ef9b3bce72bc1f1126e8
seacat271 <seacat271083@gmail.com>
2022-09-10 11:14:06 +0300

    task#2 without bgc

--- src/components/Statistics/StatList.jsx
+++ src/components/Statistics/StatList.jsx
@@ -1,26 +1,24 @@
 import PropTypes from 'prop-types';
+import css from "./StatList.module.css";
 
 export default function StatList (props) {
   const {stats} = props;
     return (
-      <ul class="stat-list">
+      <ul className={css.item__list}>
       {stats.map(item => (
-       <li class="item" key = {item.id}>
-          <span class="label">{item.label}</span>
-          <span class="percentage">{item.percentage}</span>
+       <li className={css.item} key = {item.id}>
+          <span className={css.label}>{item.label}</span>
+          <span className={css.percentage}>{item.percentage}</span>
        </li>
    ))}
     </ul>
-    
-    
       )
-  
 }
 
 StatList.propTypes = {
   stats: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.string.isRequired,
-    label: PropTypes.string,
-    percentage: PropTypes.number,
+    label: PropTypes.string.isRequired,
+    percentage: PropTypes.number.isRequired,
   })),
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import css from "./Statistics.module.css";
 
 export default function Statistics (props) {
     const {title, children} = props;
-    return (<section class="statistics">
-    {title && <h2 class="title">{title}</h2>}
+    return (<section className={css.statistics}>
+    {title && <h2 className={css.title}>{title}</h2>}
     {children}
   </section>);
 }; 
